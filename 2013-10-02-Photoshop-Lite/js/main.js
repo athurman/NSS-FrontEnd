403bfb3eb00f3b5d78a5c0f9a89d71a859d4431c
Amanda Thurman <athurman215@gmail.com>
2013-10-03 11:25:47 -0500

    PaintLite Lab

--- 2013-10-02-Photoshop-Lite/js/main.js
+++ 2013-10-02-Photoshop-Lite/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(initialize);
 
 function clearInput() {
@@ -8,6 +10,10 @@ function clearInput() {
 function initialize() {
   $('#add_color').click(addColor);
   $('#color').focus();
+  $('#add_box').click(addCanvas);
+  // $('parent_selector').on('name of event', 'child selector', name_of_function); <-- watches after initialize and when class (or other element) is dynamically added, function gets called
+  $('#colors').on('click', '.box', colorPaletteClicked);
+  $('#boxes').on('mouseover', '.canvas', colorCanvasHover);
 }
 
 function addColor() {
@@ -15,6 +21,25 @@ function addColor() {
   var $color = $('<div>');
   $color.addClass('box');
   $color.css('background-color', colorInput);
-  $('#boxes').append($color);
+  $('#colors').append($color);
+}
+
+function colorPaletteClicked() {
+  var $box = $(this);
+  $('#brush').css('background-color', $box.css('background-color'));
+}
+
+function addCanvas() {
+  var amount = parseInt($('#amount').val());
+  for(var i = 0; i < amount; i++) {
+    var $box = $('<div>');
+    $box.addClass('canvas');
+    $('#boxes').prepend($box);
+  }
   clearInput();
+}
+
+function colorCanvasHover() {
+  var $canvas = $(this);
+  $($canvas).css('background-color', $('#brush').css('background-color'));
 }
